a363aeab0249f598d4e66cd37f99562fdb063074
Kenzie Bottoms <kenziebottoms@gmail.com>
2019-12-05 14:57:50 -0600

    got intersections of paths

--- 03a.py
+++ 03a.py
@@ -20,6 +20,36 @@ def coords(directions):
     coords.append([x,y])
   return coords
 
+def paths(coords):
+  path = [[0,0]]
+  for i in range(len(coords)-1):
+    if coords[i][0] == coords[i+1][0]:
+      x = coords[i][0]
+      y1 = coords[i][1]
+      y2 = coords[i+1][1]
+      ymax = y1
+      ymin = y2
+      if ymax < ymin:
+        ymax = y2
+        ymin = y1        
+      for y in range(ymin, ymax+1):
+        path.append([x,y])
+    if coords[i][1] == coords[i+1][1]:
+      y = coords[i][1]
+      x1 = coords[i][0]
+      x2 = coords[i+1][0]
+      xmax = x1
+      xmin = x2
+      if xmax < xmin:
+        xmax = x2
+        xmin = x1        
+      for x in range(xmin, xmax+1):
+        path.append([x,y])
+  return path
+
 maps = wires("03.test.txt")
 
-print(coords(wires("03.test.txt")[0]))
+path1 = set(tuple(i) for i in paths(coords(maps[0])))
+path2 = set(tuple(i) for i in paths(coords(maps[1])))
+
+print(path1.intersection(path2))
