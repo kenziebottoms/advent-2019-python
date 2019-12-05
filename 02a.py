8c4c1c3756111421752a5a15c04412c2e0b20913
Kenzie Bottoms <kenziebottoms@gmail.com>
2019-12-05 10:53:03 -0600

    start over 02

--- 02a.py
+++ 02a.py
@@ -1,17 +1,4 @@
 def split(file, delim):
   f = open(file, "r")
-  return map(str.strip, f.read().split(delim))
+  return [int(x.strip()) for x in f.read().split(delim)]
 
-def ops(list):
-  ops = [[]]
-  separators = [1,2,99]
-  i = -1
-  for item in list:
-    if int(item) in separators:
-      i += 1
-      ops.append([])
-    ops[i].append(int(item))
-  return ops
-
-for line in ops(split("02.txt",",")):
-  print(line)
