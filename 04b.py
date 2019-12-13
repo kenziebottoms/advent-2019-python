b2c4546a0ae992533843825963a3438de7d0d584
Kenzie Bottoms <kenziebottoms@gmail.com>
2019-12-13 15:01:33 -0600

    done with part 2 of day 4

--- 04b.py
+++ 04b.py
@@ -8,16 +8,22 @@ def ascending(x):
       return False
   return True
 
-def repeats(x):
+def streaks(x):
   x = str(x)
+  streaks = []
+  count = 1
   for i in range(0, len(x)-1):
-    if int(x[i]) == int(x[i+1]):
-      return True
-  return False
+    if x[i] == x[i+1]:
+      count += 1
+    else:
+      streaks.append(count)
+      count = 1
+  streaks.append(count)
+  return streaks
 
 def validate(x):
   x = str(x)
-  return repeats(x) and ascending(x)
+  return 2 in streaks(x) and ascending(x)
 
 count = 0
 for i in range(min, max+1):
